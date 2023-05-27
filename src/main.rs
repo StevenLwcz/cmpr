d49d82a098873f96370a513aad6ee475cf2e1829
Steven Lalewicz <steven.lalewicz@gmail.com>
2023-05-27 20:46:34 +0100

    cargo fmt.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@
 // todo stdin or maybe just not bother with
 // handle invalid skip parameter
 
-use std::cmp::min;
-use std::fs::File;
-use std::io::{BufReader};
-use std::io::Read;
-use std::fs;
 use clap::{App, Arg, ArgMatches};
+use std::cmp::min;
+use std::fs;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
 use std::path::PathBuf;
 
 const DIFF_OK: i32 = 0;
@@ -19,10 +19,10 @@ const DIFF_FILE_NOT_FOUND: i32 = 2;
 const DIFF_FILE_LEN_DIFF: i32 = 3;
 
 enum Mode {
-   Hex,
-   Char,
-   Byte,
-   Single,
+    Hex,
+    Char,
+    Byte,
+    Single,
 }
 
 struct CmpOptions {
@@ -36,11 +36,20 @@ struct CmpOptions {
 impl CmpOptions {
     fn new(matches: ArgMatches) -> Self {
         Self {
-            skip: matches.value_of("skip").unwrap_or("0").parse::<usize>().unwrap_or(0),
-            mode: if matches.is_present("hex") {Mode::Hex}
-                  else if matches.is_present("char") {Mode::Char}
-                  else if matches.is_present("list") {Mode::Byte}
-                  else {Mode::Single},
+            skip: matches
+                .value_of("skip")
+                .unwrap_or("0")
+                .parse::<usize>()
+                .unwrap_or(0),
+            mode: if matches.is_present("hex") {
+                Mode::Hex
+            } else if matches.is_present("char") {
+                Mode::Char
+            } else if matches.is_present("list") {
+                Mode::Byte
+            } else {
+                Mode::Single
+            },
             file1: matches.value_of("file1").unwrap().into(),
             file2: matches.value_of("file2").map(PathBuf::from),
         }
@@ -48,29 +57,28 @@ impl CmpOptions {
 }
 
 fn main() {
-
-   let mut status = DIFF_OK;
-   let options = parse_command_line();
-   let skip = options.skip;
-   let file_name = options.file1;
-   let metadata = fs::metadata(&file_name).unwrap();
-   let len1 = metadata.len();
-   let file_name2 = options.file2.unwrap();
-   let metadata = fs::metadata(&file_name2).unwrap();
-   let len2 = metadata.len();
-   let file1 = match File::open(&file_name) {
+    let mut status = DIFF_OK;
+    let options = parse_command_line();
+    let skip = options.skip;
+    let file_name = options.file1;
+    let metadata = fs::metadata(&file_name).unwrap();
+    let len1 = metadata.len();
+    let file_name2 = options.file2.unwrap();
+    let metadata = fs::metadata(&file_name2).unwrap();
+    let len2 = metadata.len();
+    let file1 = match File::open(&file_name) {
         Ok(r) => r,
         Err(err) => {
-            eprintln!( "Can't open file {} - {}", file_name.to_string_lossy(), err);
+            eprintln!("Can't open file {} - {}", file_name.to_string_lossy(), err);
             std::process::exit(DIFF_FILE_NOT_FOUND);
-       }
+        }
     };
     let file2 = match File::open(&file_name2) {
         Ok(r) => r,
         Err(err) => {
-            eprintln!( "Can't open file {} - {}", file_name2.to_string_lossy(), err);
+            eprintln!("Can't open file {} - {}", file_name2.to_string_lossy(), err);
             std::process::exit(DIFF_FILE_NOT_FOUND);
-       }
+        }
     };
     let reader1 = BufReader::new(file1);
     let reader2 = BufReader::new(file2);
@@ -85,12 +93,19 @@ fn main() {
             status = DIFF_FAIL;
             match options.mode {
                 Mode::Single => {
-                    println!("{} {} differ at byte {}: {:02X} {:02X}", &file_name.to_string_lossy(), &file_name2.to_string_lossy(), addr, x, y);
+                    println!(
+                        "{} {} differ at byte {}: {:02X} {:02X}",
+                        &file_name.to_string_lossy(),
+                        &file_name2.to_string_lossy(),
+                        addr,
+                        x,
+                        y
+                    );
                     break;
-                },
+                }
                 Mode::Byte => println!("{:width$} {:3} {:3}", addr, x, y),
-                Mode::Hex  => println!("{:width$} {:02X} {:02X}", addr, x, y),
-                Mode::Char => println!("{:width$} {:1} {:1}", addr, to_char(x) ,to_char(y) ),
+                Mode::Hex => println!("{:width$} {:02X} {:02X}", addr, x, y),
+                Mode::Char => println!("{:width$} {:1} {:1}", addr, to_char(x), to_char(y)),
             }
         }
         addr += 1;
@@ -108,7 +123,6 @@ fn main() {
         }
     }
     std::process::exit(status);
-
 }
 
 // todo add -i ignore
@@ -120,45 +134,49 @@ fn parse_command_line() -> CmpOptions {
         .about("cmpr with ascii, byte or hex output")
         .arg(
             Arg::with_name("list")
-               .help("list all differences shown in bytes")
-               .short("l")
-               .long("list")
+                .help("list all differences shown in bytes")
+                .short("l")
+                .long("list"),
         )
         .arg(
             Arg::with_name("hex")
-               .help("list all differences shown in hex")
-               .short("h")
-               .long("hex")
+                .help("list all differences shown in hex")
+                .short("h")
+                .long("hex"),
         )
         .arg(
             Arg::with_name("char")
-               .help("list all differences shown as characters")
-               .short("c")
-               .long("char")
+                .help("list all differences shown as characters")
+                .short("c")
+                .long("char"),
         )
         .arg(
             Arg::with_name("skip")
-               .help("skip first n bytes")
-               .short("i")
-               .long("ignore")
-               .takes_value(true)
+                .help("skip first n bytes")
+                .short("i")
+                .long("ignore")
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("file1")
-            .help("first file to compare")
-            .required(true)
-            .index(1)
+                .help("first file to compare")
+                .required(true)
+                .index(1),
         )
         .arg(
             Arg::with_name("file2")
-            .help("second file to compare")
-            .index(2)
+                .help("second file to compare")
+                .index(2),
         )
         .get_matches();
 
-     CmpOptions::new(matches)
+    CmpOptions::new(matches)
 }
 
 fn to_char(b: u8) -> char {
-    if b.is_ascii_graphic() { b as char } else { '\u{25AF}' }
+    if b.is_ascii_graphic() {
+        b as char
+    } else {
+        '\u{25AF}'
+    }
 }
