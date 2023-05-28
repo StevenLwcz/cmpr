5a9738eec8e5f94a2475b604c80eecbf15c8c0da
Steven Lalewicz <steven.lalewicz@gmail.com>
2023-05-28 13:24:43 +0100

    Add test4-6.

--- tests/cli.rs
+++ tests/cli.rs
@@ -4,7 +4,7 @@ use std::fs;
 // const DIFF_OK: i32 = 0;
 const DIFF_FAIL: i32 = 1;
 // const DIFF_FILE_NOT_FOUND: i32 = 2;
-// const DIFF_FILE_LEN_DIFF: i32 = 3;
+const DIFF_FILE_LEN_DIFF: i32 = 3;
 
 #[test]
 fn test1() {
@@ -47,15 +47,52 @@ fn test3() {
         .stdout(expected);
 }
 
-// test4 test -c
-// test5 test EOF message file1
-// test6 test EOF message file2
+#[test]
+fn test4() {
+    // basic test compare 2 files and list differences: -c option
+    let testfile = "tests/expected/test4.txt";
+    let expected = fs::read_to_string(testfile).unwrap();
+    let mut cmd = Command::cargo_bin("cmpr").unwrap();
+    cmd.arg("-c")
+        .arg("tests/files/test1a.txt")
+        .arg("tests/files/test1b.txt")
+        .assert()
+        .code(DIFF_FAIL)
+        .stdout(expected);
+}
+
+#[test]
+fn test5() {
+    // test5 test EOF message file2
+    let testfile = "tests/expected/test5.txt";
+    let expected = fs::read_to_string(testfile).unwrap();
+    let mut cmd = Command::cargo_bin("cmpr").unwrap();
+    cmd.arg("-c")
+        .arg("tests/files/test2a.txt")
+        .arg("tests/files/test1b.txt")
+        .assert()
+        .code(DIFF_FILE_LEN_DIFF)
+        .stdout(expected);
+}
+
+#[test]
+fn test6() {
+    // test6 test EOF message file1
+    let testfile = "tests/expected/test6.txt";
+    let expected = fs::read_to_string(testfile).unwrap();
+    let mut cmd = Command::cargo_bin("cmpr").unwrap();
+    cmd.arg("-c")
+        .arg("tests/files/test2a.txt")
+        .arg("tests/files/test2b.txt")
+        .assert()
+        .code(DIFF_FILE_LEN_DIFF)
+        .stdout(expected);
+}
+
 // test7 test correct addr for file > 100 bytes
 // test8 test file does not exist file1
 // test9 test skip option
 // test10 test invalid skip option
 // test11 test -c with control chars if implement
 // test12 test with stdin if implement
-
-
-// test9 
+// test13 test files compare OK
